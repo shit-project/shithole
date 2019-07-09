a869147756080bc0aa6cf8f6f4dad71d35f0371f
shithole <admin@shithole.network>
2019-07-09 17:12:04 +0900

    Create prefix.go

---
+++ types/util/prefix.go
@@ -0,0 +1,16 @@
+package util
+
+const (
+	// Bech32PrefixAccAddr - Normal Address
+	Bech32PrefixAccAddr = "shit"
+	// Bech32PrefixAccPub - Public Key
+	Bech32PrefixAccPub = "shitpub"
+	// Bech32PrefixValAddr - Validator Operator's Address
+	Bech32PrefixValAddr = "shitvaloper"
+	// Bech32PrefixValPub - Validator Operator's Public Key
+	Bech32PrefixValPub = "shitvaloperpub"
+	// Bech32PrefixConsAddr - Consensus Node's Address
+	Bech32PrefixConsAddr = "shitvalcons"
+	// Bech32PrefixConsPub - Consensus Node's Public Key
+	Bech32PrefixConsPub = "shitvalconspub"
+)
